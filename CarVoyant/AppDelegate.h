d1bbe2020356d24ed5832ace8650f5dab9ec4535
Mike Holp <bathat@gmail.com>
2013-11-08 17:54:40 -0800

    Initial Commit

---
+++ CarVoyant/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CarVoyant
+//
+//  Created by Michael Holp on 11/8/13.
+//  Copyright (c) 2013 Flash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CarVoyant/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CarVoyant
+//
+//  Created by Michael Holp on 11/8/13.
+//  Copyright (c) 2013 Flash. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
